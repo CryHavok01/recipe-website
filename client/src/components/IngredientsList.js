90fafce91f482857dc9e503b416473c5e43caa65
Josh Halpern <jhhalpern@gmail.com>
2021-08-03 11:41:36 -0400

    fixed ingredient list

---
+++ client/src/components/IngredientsList.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import getOwnedIngredients from "../services/getOwnedIngredients"
+import IngredientTile from "./IngredientTile"
+
+const IngredientsList = (props) => {
+  const [ownedIngredients, setOwnedIngredients] = useState([])
+
+  const fetchIngredients = async () => {
+    if(props.user) {
+      const ingredients = await getOwnedIngredients(props.user.id)
+      setOwnedIngredients(ingredients)
+    }
+  }
+
+  useEffect(() => {
+    fetchIngredients()
+  }, [props.user])
+
+  const ingredientsList = ownedIngredients.map(ingredient => {
+    return (
+      <IngredientTile 
+        key={ingredient.id}
+        ingredient={ingredient}
+      />
+    )
+  })
+
+  return (
+    <div className="callout">
+      <div>
+        <h1 className="same_line">What's in Your Cupboard?</h1>
+        <Link to="/addIngredient" className="button same_line align_right">
+          Add New Ingredient
+        </Link>
+      </div>
+      {ingredientsList}
+    </div>
+  )
+}
+
+export default IngredientsList
